9d0293b48ab9e1600bc2055a063da67e6cf43e40
Aeris <aeris.github@gmail.com>
2021-03-02 00:29:38 +0100

    Added a basic leveling system

--- Wyrobot2/Events/ClientEvents.cs
+++ Wyrobot2/Events/ClientEvents.cs
@@ -23,6 +23,37 @@ namespace Wyrobot2.Events
                 DataManager.DeleteData(args.Guild);
                 return Task.CompletedTask;
             };
+
+            client.MessageCreated += async (_, args) =>
+            {
+                if (args.Author.IsBot) return;
+                var gldData = DataManager.GetData(args.Guild);
+                
+                // LEVELING BEGIN
+                if (gldData.Leveling.Enabled)
+                {
+                    var usrData = DataManager.GetData(args.Author, args.Guild) ?? new UserData
+                    {
+                        Id = args.Message.Author.Id,
+                        GuildId = args.Guild.Id
+                    };
+
+                    // ReSharper disable once PossibleLossOfFraction
+                    usrData.Xp = (int) (usrData.Xp + args.Message.Content.Length / 2 * gldData.Leveling.Multiplier);
+
+                    if (usrData.Xp > usrData.XpToNextLevel)
+                    {
+                        usrData.Xp -= usrData.XpToNextLevel;
+                        usrData.Level += 1;
+                        await args.Channel.SendMessageAsync(gldData.Leveling.Message
+                            .Replace("{user}", args.Message.Author.Mention)
+                            .Replace("{level}", usrData.Level.ToString()));
+                    }
+                    
+                    DataManager.SaveData(usrData);
+                }
+                // LEVELING END
+            };
         }
     }
 }
