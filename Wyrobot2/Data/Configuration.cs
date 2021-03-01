848e4c5ce0d743756cc81f46fb7de24768417bf2
Aeris <aeris.github@gmail.com>
2021-03-01 17:20:58 +0100

    Added Configuration.cs class

---
+++ Wyrobot2/Data/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Wyrobot2.Data
+{
+    public static class Configuration
+    {
+        static Configuration()
+        {
+            if (!File.Exists("config.json"))
+                return;
+
+            var json = JObject.Parse(File.ReadAllText("appsettings.json"));
+
+            DDevelopment = (string) json["DiscordTokens"]?["Development"];
+            DProduction = (string) json["DiscordTokens"]?["Production"];
+            DUseProduction = (bool) json["DiscordTokens"]?["UseProduction"];
+        }
+
+        public static string DDevelopment { get; }
+        public static string DProduction { get; }
+        public static bool DUseProduction { get; }
+    }
+}
