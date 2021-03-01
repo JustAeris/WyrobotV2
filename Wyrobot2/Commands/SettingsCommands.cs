dd03fed70af19d86b78c585b7249cceb58eb7ead
Aeris <aeris.github@gmail.com>
2021-03-01 18:11:44 +0100

    Created SettingsCommands.cs class and added a command to change prefix

---
+++ Wyrobot2/Commands/SettingsCommands.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Wyrobot2.Data;
+using Wyrobot2.Data.Models;
+// ReSharper disable UnusedMember.Global
+
+namespace Wyrobot2.Commands
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class SettingsCommands : BaseCommandModule
+    {
+        [Command("changeprefix"), RequireUserPermissions(Permissions.Administrator)]
+        public async Task ChangePrefix(CommandContext ctx, string prefix)
+        {
+            var data = DataManager<GuildData>.GetData(new GuildData(), ctx.Guild.Id.ToString());
+            data.Prefix = prefix;
+            DataManager<GuildData>.SaveData(data);
+            await ctx.RespondAsync(":ok:");
+        }
+    }
+}
