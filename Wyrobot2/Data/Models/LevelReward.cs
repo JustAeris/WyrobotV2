ba064d8b8a313c0b06832101468c376bce83561f
Aeris <aeris.github@gmail.com>
2021-03-02 18:19:07 +0100

    Added a constructor

--- Wyrobot2/Data/Models/LevelReward.cs
+++ Wyrobot2/Data/Models/LevelReward.cs
@@ -2,6 +2,11 @@
 {
     public class LevelReward
     {
+        public LevelReward(int requiredLevel, ulong roleId)
+        {
+            RequiredLevel = requiredLevel;
+            RoleId = roleId;
+        }
         public int RequiredLevel { get; set; }
         public ulong RoleId { get; set; }
     }
