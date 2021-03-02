1c704fe847ce5952d8778fe549898b94f2462b02
Aeris <aeris.github@gmail.com>
2021-03-02 16:54:50 +0100

    Made a custom help formatter to change the embed color

---
+++ Wyrobot2/CustomHelpFormatter.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Converters;
+using DSharpPlus.CommandsNext.Entities;
+using DSharpPlus.Entities;
+
+namespace Wyrobot2
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class CustomHelpFormatter : DefaultHelpFormatter
+    {
+        public CustomHelpFormatter(CommandContext ctx) : base(ctx) { }
+
+        public override CommandHelpMessage Build()
+        {
+            EmbedBuilder.Color = DiscordColor.DarkButNotBlack;
+            return base.Build();
+        }
+    }
+    
+}
