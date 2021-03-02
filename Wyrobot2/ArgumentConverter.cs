b81ace3de2940dc52589842c93de71116b7aa550
Aeris <aeris.github@gmail.com>
2021-03-02 16:53:36 +0100

    Made a custom argument converter for bool values

---
+++ Wyrobot2/ArgumentConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Converters;
+using DSharpPlus.Entities;
+
+namespace Wyrobot2
+{
+    internal class CustomArgumentConverter : IArgumentConverter<bool>
+    {
+        public Task<Optional<bool>> ConvertAsync(string value, CommandContext ctx)
+        {
+            if (bool.TryParse(value, out var boolean))
+            {
+                return Task.FromResult(Optional.FromValue(boolean));
+            }           
+
+            switch (value?.ToLower())
+            {
+                case "yes":
+                case "y":
+                case "t":
+                case "true":
+                    return Task.FromResult(Optional.FromValue(true));
+
+                case "no":
+                case "n":
+                case "f":
+                case "false":
+                    return Task.FromResult(Optional.FromValue(false));
+
+                default:
+                    return Task.FromResult(Optional.FromNoValue<bool>());
+            } 
+        }   
+    }
+}
