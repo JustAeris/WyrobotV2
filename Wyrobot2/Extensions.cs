181d0cd2ba33d1217b0de0fd50e9442d5ff36983
Aeris <aeris.github@gmail.com>
2021-03-04 11:08:13 +0100

    Added an extension.

---
+++ Wyrobot2/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using DSharpPlus.Entities;
+
+namespace Wyrobot2
+{
+    public static class Extensions
+    {
+        public static bool CanPunish(this DiscordMember mbr1, DiscordMember mbr2) => mbr1.Hierarchy > mbr2.Hierarchy;
+    }
+}
