5a4da81bbdc3832b7e1217cd6208a981c26ade11
Aeris <aeris.github@gmail.com>
2021-03-04 17:39:14 +0100

    Added a kick command.

--- Wyrobot2/Commands/ModerationCommands.cs
+++ Wyrobot2/Commands/ModerationCommands.cs
@@ -17,7 +17,7 @@ namespace Wyrobot2.Commands
     // ReSharper disable once ClassNeverInstantiated.Global
     public class ModerationCommands : BaseCommandModule
     {
-        [Command("ban"), RequirePermissions(Permissions.BanMembers), Priority(2), Description("Permanently bans a member.")]
+        [Command("ban"), RequirePermissions(Permissions.BanMembers), Priority(2), Description("Bans a member.")]
         public async Task Ban(CommandContext ctx, [Description("Member to ban.")] DiscordMember member, [Description("Duration of the ban.")] TimeSpan expiresIn, [RemainingText, Description("Reason of the ban.")] string reason = "No reason provided.")
         {
             if (!ctx.Member.CanPunish(member))
@@ -45,7 +45,7 @@ namespace Wyrobot2.Commands
 
             try
             {
-                await member.SendMessageAsync("You have benn " +
+                await member.SendMessageAsync("You have been " +
                                               $"{(expiresIn == TimeSpan.MaxValue ? "permanently banned" : $"banned for {(expiresIn > TimeSpan.FromDays(1) ? $"{expiresIn.TotalDays} days" : $"{expiresIn.TotalHours} hours")}")} " +
                                               $"from **{ctx.Guild.Name}** for the following reason: ```{reason}```");
                 await ctx.Guild.BanMemberAsync(member, 0, $"'{ctx.Member.Username}#{ctx.Member.Discriminator}' banned '{member.Username}#{member.Discriminator}'. " + reason);
@@ -69,7 +69,7 @@ namespace Wyrobot2.Commands
             await ctx.RespondAsync(new DiscordEmbedBuilder()
                 .WithTitle(":hammer: Success!")
                 .WithDescription($"{member.Username}#{member.Discriminator} has been " +
-                                 $"{(expiresIn == TimeSpan.MaxValue ? "permanently banned" : $"banned for {(expiresIn > TimeSpan.FromDays(1) ? $"{expiresIn.TotalDays} days" : $"{expiresIn.TotalHours} hours")}")}" +
+                                 $"{(expiresIn == TimeSpan.MaxValue ? "permanently **banned**" : $"**banned** for {(expiresIn > TimeSpan.FromDays(1) ? $"{expiresIn.TotalDays} days" : $"{expiresIn.TotalHours} hours")}")}" +
                                  $"{(reason == null ? null : $" for the following reason: ```{reason}```")}")
                 .WithColor(DiscordColor.DarkButNotBlack)
                 .WithThumbnail(member.AvatarUrl)
@@ -80,5 +80,63 @@ namespace Wyrobot2.Commands
         [Command("ban"), RequirePermissions(Permissions.BanMembers), Priority(1)]
         public async Task Ban(CommandContext ctx, [Description("Member to ban.")] DiscordMember member, [RemainingText, Description("Reason of the ban.")] string reason = "No reason provided.") =>
             await Ban(ctx, member, TimeSpan.MaxValue, reason);
+        
+        [Command("kick"), RequirePermissions(Permissions.KickMembers), Description("Kicks a member.")]
+        public async Task Kick(CommandContext ctx, [Description("Member to kick.")] DiscordMember member, [RemainingText, Description("Reason of the kick.")] string reason = "No reason provided.")
+        {
+            if (!ctx.Member.CanPunish(member))
+            {
+                await ctx.RespondAsync(new DiscordEmbedBuilder
+                {
+                    Title = "Permission denied",
+                    Description =
+                        $"{DiscordEmoji.FromName(ctx.Client, ":raised_hand:")} You lack permissions necessary to run this command.",
+                    Color = new DiscordColor(0xFF0000)
+                });
+            }
+            
+            var usrData = DataManager.GetData(member, ctx.Guild) ?? new UserData{ Id = member.Id, GuildId = ctx.Guild.Id };
+            usrData.Sanctions ??= new List<Sanction>();
+            
+            usrData.Sanctions.Add(new Sanction
+            {
+                Type = Sanction.SanctionType.Kick,
+                PunisherId = ctx.Member.Id,
+                IssuedAt = DateTimeOffset.Now,
+                ExpiresAt = DateTimeOffset.Now,
+                Reason = reason
+            });
+
+            try
+            {
+                await member.SendMessageAsync($"You have kicked from **{ctx.Guild.Name}** for the following reason: ```{reason}```");
+                await member.RemoveAsync($"'{ctx.Member.Username}#{ctx.Member.Discriminator}' kicked '{member.Username}#{member.Discriminator}'. " + reason);
+            }
+            catch (Exception e)
+            {
+                ctx.Client.Logger.LogError(EventIds.Error , $"An error occured while trying to kick '{member.Username}#{member.Discriminator}'. Exception: {e}");
+                
+                await ctx.RespondAsync(new DiscordEmbedBuilder()
+                    .WithTitle("Oops! An error occured.")
+                    .WithDescription(
+                        $"An error occured while trying to ban '{member.Username}#{member.Discriminator}'. Be sure the bot has enough permissions and retry again.")
+                    .WithColor(DiscordColor.DarkRed)
+                    .WithFooter($"Issuer ID: {ctx.User.Id}")
+                    .WithTimestamp(DateTime.UtcNow));
+            }
+            
+            DataManager.SaveData(usrData);
+            ctx.Client.Logger.LogInformation(EventIds.Kick , $"'{ctx.Member.Username}#{ctx.Member.Discriminator}' kicked '{member.Username}#{member.Discriminator}' for the following reason: {reason}.");
+            
+            await ctx.RespondAsync(new DiscordEmbedBuilder()
+                .WithTitle(":hammer: Success!")
+                .WithDescription($"{member.Username}#{member.Discriminator} has been **kicked**{(reason == null ? null : $" for the following reason: ```{reason}```")}")
+                .WithColor(DiscordColor.DarkButNotBlack)
+                .WithThumbnail(member.AvatarUrl)
+                .WithFooter($"Issuer ID: {ctx.User.Id}")
+                .WithTimestamp(DateTime.UtcNow));
+        }
+        
+        
     }
 }
