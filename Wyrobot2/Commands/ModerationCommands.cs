b58f48b66d4ec7c2c28c2d0a2725cd19af5273c0
Aeris <aeris.github@gmail.com>
2021-03-05 11:21:40 +0100

    Added warn command

--- Wyrobot2/Commands/ModerationCommands.cs
+++ Wyrobot2/Commands/ModerationCommands.cs
@@ -111,7 +111,7 @@ namespace Wyrobot2.Commands
 
             try
             {
-                await member.SendMessageAsync($"You have kicked from **{ctx.Guild.Name}** for the following reason: ```{reason}```");
+                await member.SendMessageAsync($"You have been kicked from **{ctx.Guild.Name}** for the following reason: ```{reason}```");
                 await member.RemoveAsync($"'{ctx.Member.Username}#{ctx.Member.Discriminator}' kicked '{member.Username}#{member.Discriminator}'. " + reason);
             }
             catch (Exception e)
@@ -139,6 +139,51 @@ namespace Wyrobot2.Commands
                 .WithTimestamp(DateTime.UtcNow));
         }
         
-        
+        [Command("warn"), RequirePermissions(Permissions.ManageMessages), Description("Warns a member.")]
+        public async Task Warn(CommandContext ctx, [Description("Member to warn.")] DiscordMember member, [RemainingText, Description("Reason of the warn.")] string reason = "No reason provided.")
+        {
+            if (!ctx.Member.CanPunish(member))
+            {
+                await ctx.RespondAsync(new DiscordEmbedBuilder
+                {
+                    Title = "Permission denied",
+                    Description =
+                        $"{DiscordEmoji.FromName(ctx.Client, ":raised_hand:")} You lack permissions necessary to run this command.",
+                    Color = new DiscordColor(0xFF0000)
+                });
+            }
+            
+            var usrData = DataManager.GetData(member, ctx.Guild) ?? new UserData{ Id = member.Id, GuildId = ctx.Guild.Id };
+            usrData.Sanctions ??= new List<Sanction>();
+            
+            usrData.Sanctions.Add(new Sanction
+            {
+                Type = Sanction.SanctionType.Warn,
+                PunisherId = ctx.Member.Id,
+                IssuedAt = DateTimeOffset.Now,
+                ExpiresAt = DateTimeOffset.Now + TimeSpan.FromDays(1),
+                Reason = reason
+            });
+
+            try
+            {
+                await member.SendMessageAsync($"You have been warned from **{ctx.Guild.Name}** for the following reason: ```{reason}```");
+            }
+            catch
+            {
+                // ignored
+            }
+            
+            DataManager.SaveData(usrData);
+            ctx.Client.Logger.LogInformation(EventIds.Kick , $"'{ctx.Member.Username}#{ctx.Member.Discriminator}' warned '{member.Username}#{member.Discriminator}' for the following reason: {reason}.");
+            
+            await ctx.RespondAsync(new DiscordEmbedBuilder()
+                .WithTitle(":hammer: Success!")
+                .WithDescription($"{member.Username}#{member.Discriminator} has been **warned**{(reason == null ? null : $" for the following reason: ```{reason}```")}")
+                .WithColor(DiscordColor.DarkButNotBlack)
+                .WithThumbnail(member.AvatarUrl)
+                .WithFooter($"Issuer ID: {ctx.User.Id}")
+                .WithTimestamp(DateTime.UtcNow));
+        }
     }
 }
