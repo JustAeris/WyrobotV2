cde241c175dc6265b4eaff615104a22358056b86
Aeris <aeris.github@gmail.com>
2021-03-11 18:00:52 +0100

    Added a recurrent task class

---
+++ Wyrobot2/Events/RecurrentEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Timers;
+using DSharpPlus;
+
+namespace Wyrobot2.Events
+{
+    public static class RecurrentEvent
+    {
+        private static readonly Timer Timer;
+        private static DiscordClient _client;
+
+        static RecurrentEvent()
+        {
+            Timer = new Timer
+            {
+                Enabled = true,
+                AutoReset = true,
+                Interval = 60000
+            };
+            Timer.Elapsed += TimerOnElapsed;
+        }
+
+        public static void InitializeAndStart(DiscordClient client)
+        {
+            _client = client;
+            Timer.Start();
+        }
+
+        private static void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
