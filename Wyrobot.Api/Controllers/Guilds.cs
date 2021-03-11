2cb7cd25d6d2d2b65e70f6f21d7d8dffe5ccb7d2
Aeris <aeris.github@gmail.com>
2021-03-11 17:55:26 +0100

    Added API versioning and renamed parameter

--- Wyrobot.Api/Controllers/Guilds.cs
+++ Wyrobot.Api/Controllers/Guilds.cs
@@ -1,5 +1,4 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Wyrobot.Api.Data;
 using Wyrobot.Api.Models;
@@ -11,10 +10,10 @@ namespace Wyrobot.Api.Controllers
     public class Guilds : Controller
     {
         // GET
-        [Route("api/[controller]/{userId}/{id}")]
-        public async Task<object> GetGuildSettings(string apiKey, ulong id, ulong userId)
+        [Route("v1/[controller]/{userId}/{id}")]
+        public async Task<object> GetGuildSettings(string key, ulong id, ulong userId)
         {
-            if (!Token.Authorize(apiKey, false))
+            if (!Token.Authorize(key, false))
                 return new ApiResponse
                 {
                     Status = 401,
