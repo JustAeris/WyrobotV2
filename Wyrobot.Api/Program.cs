df9c406bfac040fc7ad693fbc0f3c9ad56ec787b
Aeris <aeris.github@gmail.com>
2021-03-11 17:59:24 +0100

    Code clean-up and added a way to get the daily API key

--- Wyrobot.Api/Program.cs
+++ Wyrobot.Api/Program.cs
@@ -1,18 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Wyrobot.Api.Data;
 
 namespace Wyrobot.Api
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
+            if (args[0] == "getkey") Console.WriteLine(Token.Generate(false));
             CreateHostBuilder(args).Build().Run();
         }
 
