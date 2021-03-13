fc759e0f334d67d291f3ed74ce128eff78b0c406
Aeris <aeris.github@gmail.com>
2021-03-13 10:36:27 +0100

    Added a system to handle temporary sanctions

--- Wyrobot2/Events/RecurrentEvent.cs
+++ Wyrobot2/Events/RecurrentEvent.cs
@@ -1,5 +1,10 @@
-﻿using System.Timers;
+﻿using System;
+using System.Timers;
 using DSharpPlus;
+using Microsoft.Extensions.Logging;
+using Wyrobot2.Data;
+using Wyrobot2.Data.Models;
+// ReSharper disable TemplateIsNotCompileTimeConstantProblem
 
 namespace Wyrobot2.Events
 {
@@ -25,9 +30,52 @@ namespace Wyrobot2.Events
             Timer.Start();
         }
 
-        private static void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        private static async void TimerOnElapsed(object sender, ElapsedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            foreach (var guild in _client.Guilds.Values)
+            {
+                var userDataList = DataManager.GetAllData(guild);
+                var guildData = DataManager.GetData(guild);
+
+                foreach (var data in userDataList)
+                {
+                    foreach (var sanction in data.Sanctions)
+                    {
+                        if (!sanction.HasExpired) continue;
+                        
+                        if (sanction.HasExpired && sanction.HasBeenHandled) continue;
+
+                        
+                        switch (sanction.Type)
+                        {
+                            case Sanction.SanctionType.Ban:
+                                await guild.UnbanMemberAsync(data.Id, $"'{data.Id}' has been automatically unbanned.");
+                                _client.Logger.LogInformation(EventIds.Unban, $"'{data.Id}' has been automatically unbanned");
+                                sanction.HasBeenHandled = true;
+                                break;
+                            
+                            case Sanction.SanctionType.Mute:
+                                var member = await guild.GetMemberAsync(data.Id);
+                                var muteRole = guild.GetRole(guildData.Moderation.MuteRoleId);
+                                await member.RevokeRoleAsync(muteRole, $"{member.Username}#{member.Discriminator} has been automatically un-muted.");
+                                _client.Logger.LogInformation(EventIds.Unmute, $"{member.Username}#{member.Discriminator} has been automatically un-mute");
+                                sanction.HasBeenHandled = true;
+                                break;
+                            
+                            case Sanction.SanctionType.Warn:
+                            case Sanction.SanctionType.Kick:
+                            case Sanction.SanctionType.Unban:
+                                continue;
+                            
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                    DataManager.SaveData(data);
+                }
+            }
+            
+            _client.Logger.LogInformation(EventIds.Scheduled, "Scheduled tasks have been executed successfully");
         }
     }
 }
