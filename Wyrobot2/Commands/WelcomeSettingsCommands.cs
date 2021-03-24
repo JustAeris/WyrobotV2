702b8bbf895c722e4f645cd1de8c8b00c34e0540
Aeris <aeris.github@gmail.com>
2021-03-24 20:29:40 +0100

    Added commands to configure welcoming

---
+++ Wyrobot2/Commands/WelcomeSettingsCommands.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Wyrobot2.Data;
+// ReSharper disable UnusedMember.Global
+
+namespace Wyrobot2.Commands
+{
+    [Group("welcomesettings")]
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class WelcomeSettingsCommands : BaseCommandModule
+    {
+        [Command("enabled"), Description("Enable / Disable leveling. This will not affect existing data."), RequireUserPermissions(Permissions.Administrator)]
+        public async Task Enabled(CommandContext ctx, bool value)
+        {
+            var gldData = DataManager.GetData(ctx.Guild);
+            var oldValue = gldData.Welcome.Enabled;
+            gldData.Welcome.Enabled = value;
+            DataManager.SaveData(gldData);
+            await ctx.RespondAsync(new DiscordEmbedBuilder()
+                .WithTitle(":white_check_mark: Success!")
+                .WithDescription($"The welcoming system has been turned from **{oldValue}** to **{value}**")
+                .WithColor(DiscordColor.DarkButNotBlack)
+                .WithFooter($"Issuer ID: {ctx.User.Id}")
+                .WithTimestamp(DateTime.UtcNow)
+                .Build());
+        }
+        
+        [Command("message"), Description("Change the welcome message."), RequireUserPermissions(Permissions.Administrator)]
+        public async Task Message(CommandContext ctx, [RemainingText, Description("Message to display when a user joins the server. Use `{user}` to mention the new member.")] string value)
+        {
+            var data = DataManager.GetData(ctx.Guild);
+            var oldValue = data.Welcome.Message;
+            data.Welcome.Message = value;
+            DataManager.SaveData(data);
+            await ctx.RespondAsync(new DiscordEmbedBuilder()
+                .WithTitle(":white_check_mark: Success!")
+                .WithDescription($"The welcome message has been changed from **{oldValue}** to **{value}**")
+                .WithColor(DiscordColor.DarkButNotBlack)
+                .WithFooter($"Issuer ID: {ctx.User.Id}")
+                .WithTimestamp(DateTime.UtcNow)
+                .Build());
+        }
+        
+        [Command("channel"), Description("Change the welcome channel."), RequireUserPermissions(Permissions.Administrator)]
+        public async Task Channel(CommandContext ctx, [RemainingText, Description("Channel to send the welcome message to. Mention a channel.")] DiscordChannel value)
+        {
+            var data = DataManager.GetData(ctx.Guild);
+            var oldValue = data.Welcome.ChannelId;
+            data.Welcome.ChannelId = value.Id;
+            DataManager.SaveData(data);
+            await ctx.RespondAsync(new DiscordEmbedBuilder()
+                .WithTitle(":white_check_mark: Success!")
+                .WithDescription($"The welcome message has been changed from **{(oldValue == 0 ? "None" : ctx.Guild.GetChannel(oldValue) == null ? "None" : ctx.Guild.GetChannel(oldValue).Mention)}** to **{value}**")
+                .WithColor(DiscordColor.DarkButNotBlack)
+                .WithFooter($"Issuer ID: {ctx.User.Id}")
+                .WithTimestamp(DateTime.UtcNow)
+                .Build());
+        }
+    }
+}
--- Wyrobot2/Program.cs
+++ Wyrobot2/Program.cs
@@ -69,6 +69,7 @@ namespace Wyrobot2
             commands.RegisterCommands<LevelingSettingsCommands>();
             commands.RegisterCommands<LevelRewardsSettingsCommands>();
             commands.RegisterCommands<ModerationCommands>();
+            commands.RegisterCommands<WelcomeSettingsCommands>();
             
             SanctionHandler.InitializeAndStart(_client);
             
