2bada1e79ba63a0dbc89eb3686cf559bc38004e4
Aeris <aeris.github@gmail.com>
2021-04-01 15:12:01 +0200

    Added lavalink configuration and refactored Configuration.cs

--- Wyrobot2/Data/Configuration.cs
+++ Wyrobot2/Data/Configuration.cs
@@ -12,13 +12,45 @@ namespace Wyrobot2.Data
 
             var json = JObject.Parse(File.ReadAllText("appsettings.json"));
 
-            DDevelopment = (string) json["DiscordTokens"]?["Development"];
-            DProduction = (string) json["DiscordTokens"]?["Production"];
-            DUseProduction = (bool) json["DiscordTokens"]?["UseProduction"];
+            Discord = new DiscordConfiguration(
+                (string) json["DiscordTokens"]?["Development"],
+                (string) json["DiscordTokens"]?["Production"],
+                (bool) json["DiscordTokens"]?["UseProduction"]);
+            
+            Lavalink = new LavalinkConfiguration(
+                (string) json[nameof(Lavalink)]?["Host"],
+                (int) json[nameof(Lavalink)]?["Port"],
+                (string) json[nameof(Lavalink)]?["Password"]);
         }
 
-        public static string DDevelopment { get; }
-        public static string DProduction { get; }
-        public static bool DUseProduction { get; }
+        public static DiscordConfiguration Discord { get; }
+        public static LavalinkConfiguration Lavalink { get; set; }
+
+        public class DiscordConfiguration
+        {
+            public DiscordConfiguration(string development, string production, bool useProduction)
+            {
+                Development = development;
+                Production = production;
+                UseProduction = useProduction;
+            }
+            public string Development { get; }
+            public string Production { get; }
+            public bool UseProduction { get; }
+        }
+
+        public class LavalinkConfiguration
+        {
+            public LavalinkConfiguration(string host, int port, string password)
+            {
+                Host = host;
+                Port = port;
+                Password = password;
+            }
+
+            public string Host { get; set; }
+            public int Port { get; set; }
+            public string Password { get; set; }
+        }
     }
 }
